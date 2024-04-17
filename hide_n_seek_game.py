691b641ea50cf8903dc7650a62f1f104e5788f83
Keegan Mboya <129076951+KEEGANTE@users.noreply.github.com>
2024-04-17 16:10:55 +0300

    Create hide_n_seek_game.py
    This is a python program that simulates a hide and seek game

---
+++ hide_n_seek_game.py
@@ -0,0 +1,7 @@
+import time
+
+for i in range (6): # Write a for loop that counts to five.
+    print(i, "Mississippi") # Body of the loop - print the loop iteration number and the word "Mississippi".
+    time.sleep(1) # Body of the loop - use: time.sleep(1) # Sleep for 1 second
+    
+print("Ready or not, here I come!")
